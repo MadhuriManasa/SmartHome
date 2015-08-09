b1f45123d41b3b305f8486e1ff65b66cf5252f2f
Bharath Booshan <bharath2020@gmail.com>
2015-08-08 17:45:08 -0700

    requirements

--- smarthomemongo.py
+++ smarthomemongo.py
@@ -47,8 +47,7 @@ class SmartHomeDB:
 	def __init__(self):
 		self.client = client = MongoClient(mongolab_uri,
                      connectTimeoutMS=30000,
-                     socketTimeoutMS=None,
-                     socketKeepAlive=True)
+                     socketTimeoutMS=None)
 		self.db = client.get_default_database()
 		self.temperature_collection = self.db['temperature']
 
