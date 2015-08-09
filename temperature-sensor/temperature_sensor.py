57d71058e6611b412dac82f71a002fdff9cdd2bd
Bharath Booshan <bharath2020@gmail.com>
2015-08-08 22:57:00 -0700

    temperature sensor

--- temperature-sensor/temperature_sensor.py
+++ temperature-sensor/temperature_sensor.py
@@ -1,21 +1,20 @@
-#from Adafruit_Python_DHT  import Adafruit_DHT
+from Adafruit_Python_DHT  import Adafruit_DHT
 import time
 import json
 import httplib
 
 URL='bb-smart-home.herokuapp.com'
 
-# def temperature_humidity(sensor,pin):
-# 	humidity, temperature = Adafruit_DHT.read_retry(sensor,pin)
-# 	if humidity is not None and temperature is not None:
-# 		return [temperature,humidity]
-# 	else:
-# 		return []
+def temperature_humidity(sensor,pin):
+	humidity, temperature = Adafruit_DHT.read_retry(sensor,pin)
+	if humidity is not None and temperature is not None:
+		return [temperature,humidity]
+	else:
+		return []
 
 
 def upload_temp():
-	#points = temperature_humidity(22,17)
-	points = [57.0,66.0]
+	points = temperature_humidity(22,17)
 	if len(points) != 0:
 		record = {
 		'temperature' : points[0],
@@ -25,7 +24,7 @@ def upload_temp():
 
 		}
 		conn = httplib.HTTPConnection(URL)
-		conn.request('POST',URL+'/add',json.dumps(record),{'Content-Type':'application/json'})
+		conn.request("POST",'/add',json.dumps(record),{'Content-Type':'application/json'})
 		response = conn.getresponse()
 		print response.status, response.reason
 		conn.close()
