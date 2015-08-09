18ec1093ad327fae31c0b057e7caad1dbbf90b5c
Bharath Booshan <bharath2020@gmail.com>
2015-08-08 17:07:51 -0700

    Temeperature

---
+++ Temperature/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn temperature_server:app --log-file=-
--- Temperature/temperature_server.py
+++ Temperature/temperature_server.py
@@ -1,52 +1,28 @@
-from flask import Flask
+#!/usr/bin/env python3
+from flask import Flask, request
 import os
-import RPi.GPIO
+import json
 import time
-from temperature import temperature_humidity
-
+from smarthomemongo import SmartHomeDB
 
 app = Flask(__name__)
-
-def setup_board():
-	RPi.GPIO.setmode(RPi.GPIO.BOARD)
-	RPi.GPIO.setup(11,RPi.GPIO.IN)
-	RPi.GPIO.setup(11,RPi.GPIO.OUT)
-	RPi.GPIO.output(11,False)
-	print "Board Setup"
+smartDB = SmartHomeDB()
 
 
-@app.route('/ON')
-def turn_on():
-	result = ""
-	try:
-		RPi.GPIO.output(11,True)
-		time.sleep(0.1)
-		result = str(RPi.GPIO.input(11))
-	except Exception as inst:
-		result = str(inst)
-	return result 
+@app.route('/')
+def index():
+	records = smartDB.getTemperaturePoints()
+	print(records)
+	return json.dumps(records)
 
-@app.route('/OFF')
-def turn_off():
-	RPi.GPIO.output(11,False)
-	time.sleep(0.1)
-	return str(RPi.GPIO.input(11))
+@app.route('/add', methods=['POST'])
+def add():
+	recordJson = request.get_json()
+	smartDB.insertTemperaturePoint(recordJson)
+	return 'Success', 200
 
-
-@app.route('/status')
-def status():
-	input_status = RPi.GPIO.input(11)
-	return str(input_status)
-
-
-@app.route('/temperature')
-def temperature():
-	return temperature_humidity(22,17)
-
-    
 
 if __name__ == '__main__':
-	setup_board()
 	app.run(host='0.0.0.0',debug=True)
 
 
